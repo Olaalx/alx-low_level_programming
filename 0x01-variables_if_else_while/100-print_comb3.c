a294866d4de21c022383341a7ebe0cd77359bd06
Olaalx <olaazez3031@gmail.com>
2023-09-08 18:13:35 -0700

    done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ int main(void)
 {
 	int digit1 = 0, digit2;
 
-	while (digit <= 9)
+	while (digit1 <= 9)
 	{
 		digit2 = 0;
 		while (digit2 <= 9)
@@ -33,7 +33,7 @@ int main(void)
 		}
 		digit1++;
 	}
-	putchar("\n");
+	putchar('\n');
 
 	return (0);
 }
