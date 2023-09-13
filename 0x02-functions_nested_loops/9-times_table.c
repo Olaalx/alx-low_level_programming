c7d454d4ce5bdb3abb902baace03810a389d9b34
Olaalx <olaazez3031@gmail.com>
2023-09-12 21:50:49 -0700

    c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -32,7 +32,7 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + 48); /*get thr first digit*/
-			_putchar((prod % 10 + 48); /*get the second digit*/
+			_putchar((prod % 10 + 48)); /*get the second digit*/
 		}
 	_putchar('\n');
 	}
