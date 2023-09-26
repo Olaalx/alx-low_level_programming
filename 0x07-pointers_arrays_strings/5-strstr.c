8ed1c6d2b099cb5c129cbe151146c0e313fa9291
Olaalx <olaazez3031@gmail.com>
2023-09-25 18:12:42 -0700

    0x07 p,a,s

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,4 +2,27 @@
 
 /**
  * *_strstr - function that locates a substring.
- * */
+ * @haystack: input
+ * @needle: input
+ * Return: 0
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*| == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+			return (hayystack);
+	}
+
+	return (0);
+}
