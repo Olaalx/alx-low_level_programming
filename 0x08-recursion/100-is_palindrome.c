148596c774d99b03cbae1704867a89a1274f9aaf
Olaalx <olaazez3031@gmail.com>
2023-09-28 13:05:04 -0700

    0x08-recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,20 +1,7 @@
 #include "main.h"
 
-/**
- * length_string - return length of string
- * @s: string
- * Return: int
-*/
-int is_palindrome(char *s);
-int check(char *s, int b, int e, int o);
-int length_string(char *s)
-{
-	int n = 0;
-
-	if (*s > '\0')
-		n += length_string(s + 1) + 1;
-	return (n);
-}
+int check(char *s, int a, int b);
+int length_string(char *s);
 
 /**
  * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
@@ -24,26 +11,37 @@ int length_string(char *s)
 
 int is_palindrome(char *s)
 {
-	int e = length_string(s);
+	if (*s == 0)
+		return (0);
+	return (check(s, 0, length_string(s)));
+}
 
-	return (check(s, 0, e - 1, e % 2));
+/**
+ * length_string - return length of string
+ * @s: string
+ * Return: int
+*/
+
+int length_string(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + length_string(s + 1));
 }
 
 /**
  * check - check string
  * @s: string
- * @b: int move from right to left
- * @e: int move from left to right
- * @o: int
+ * @a: int
+ * @b: length
  * Return: 0 or 1
 */
 
-int check(char *s, int b, int e, int o)
+int check(char *s, int a, int b)
 {
-	if ((b == e && o != 0) || (b == e + 1 && o == 0))
-		return (1);
-	else if (s[b] != s[e])
+	if (*(s + a) != *(s + b - 1))
 		return (0);
-	else
-		return (check(s, b, e - 1, o));
+	else if (a >= b)
+		return (1);
+	return (check(s, a + 1, b - 1));
 }
